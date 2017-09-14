e800875070275efb8839b5c1d76ed5e3d759ad02
Andy Cook <cookab1@appstate.edu>
2017-09-14 17:20:55 -0400

    Andy's init commit

---
+++ app/src/main/java/com/bignerdranch/android/officecrime/Crime.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.officecrime;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by cookab1 on 9/14/2017.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+}
