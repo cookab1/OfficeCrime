e61dd43c2fbc5c4890f61da632ef6e6835c88f6d
Andy Cook <cookab1@appstate.edu>
2017-09-18 21:39:13 -0400

    Made SingleFragmentActivity.java and it now has the import error

---
+++ app/src/main/java/com/bignerdranch/android/officecrime/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.officecrime;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 9/18/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
