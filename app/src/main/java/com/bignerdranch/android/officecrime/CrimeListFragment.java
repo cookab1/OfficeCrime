9ddbe8ffcf15c3030830c78c45fbb36543a3f1a0
Andy Cook <cookab1@appstate.edu>
2017-10-02 12:59:02 -0400

    Made my implicit intents without using them

--- app/src/main/java/com/bignerdranch/android/officecrime/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/officecrime/CrimeListFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -52,7 +53,9 @@ public class CrimeListFragment extends Fragment {
         public void bind(Crime crime) {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
-            mDateTextView.setText(mCrime.getDate().toString());
+            String dateFormat = "EEE, MMM dd";
+            String dateString = DateFormat.format(dateFormat, mCrime.getDate()).toString();
+            mDateTextView.setText(dateString);
             mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 
